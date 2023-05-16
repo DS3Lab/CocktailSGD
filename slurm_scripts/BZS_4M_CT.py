7e03ca03ca34661c2f394c445c8cbf3763485a1c
Jue Wang <zjuwangjue@gmail.com>
2023-05-16 12:32:54 +0000

    update

--- slurm_scripts/BZS_4M_CT.py
+++ slurm_scripts/BZS_4M_CT.py
@@ -53,10 +53,10 @@ rp_wikipedia:0.04 \
 --checkpoint-path /work/data/model_ckpts/$WANDB_NAME \
 --num-layers {{N_LAYER_PER_DEVICE}} --embedding-dim 4096 \
 --initial-loss-scale 512 \
---total-steps 238418 --warmup-steps 100 --train-warmup-steps 0 \
+--total-steps 238418 --warmup-steps 10 --train-warmup-steps 0 \
 --stop-steps 238419 \
---checkpoint-steps 100 \
---lr 1e-5 --seq-length 2048 --batch-size 4 --micro-batch-size 1 --gradient-accumulate-step 4 \
+--checkpoint-steps 500 \
+--lr 1e-5 --seq-length 2048 --batch-size 32 --micro-batch-size 1 --gradient-accumulate-step 1 \
 --dist-url tcp://${master_ip}:7026 \
 --world-size $(({{PP_DEGREE}}*{{DP_DEGREE}})) --pipeline-group-size {{PP_DEGREE}} --data-group-size {{DP_DEGREE}} \
 --job-id {{JOB_ID}} --net-interface ${netif} \
@@ -67,21 +67,21 @@ rp_wikipedia:0.04 \
 
 (trap 'kill 0' SIGINT; \
 RP_PREFIX=/work/data/data_0 python -u dist_lm_sharded_train.py $(echo ${ARGS}) --cuda-id 0 --rank 0 \
-    & \
+    ; \
 RP_PREFIX=/work/data/data_0 python -u dist_lm_sharded_train.py $(echo ${ARGS}) --cuda-id 1 --rank 0 \
-    & sleep 0.5 & \
+    ; \
 RP_PREFIX=/work/data/data_1 python -u dist_lm_sharded_train.py $(echo ${ARGS}) --cuda-id 2 --rank 0 \
-    & \
+    ; \
 RP_PREFIX=/work/data/data_1 python -u dist_lm_sharded_train.py $(echo ${ARGS}) --cuda-id 3 --rank 0 \
-    & sleep 0.5 & \
+    ; sleep 1 ; \
 RP_PREFIX=/work/data/data_2 python -u dist_lm_sharded_train.py $(echo ${ARGS}) --cuda-id 4 --rank 0 \
-    & \
+    ; \
 RP_PREFIX=/work/data/data_2 python -u dist_lm_sharded_train.py $(echo ${ARGS}) --cuda-id 5 --rank 0 \
-    & sleep 0.5 & \
+    ; \
 RP_PREFIX=/work/data/data_3 python -u dist_lm_sharded_train.py $(echo ${ARGS}) --cuda-id 6 --rank 0 \
-    & \
+    ; \
 RP_PREFIX=/work/data/data_3 python -u dist_lm_sharded_train.py $(echo ${ARGS}) --cuda-id 7 --rank 0 \
-    & sleep 0.5 & \
+    ; sleep 1 ; \
 wait)
 
 
@@ -90,9 +90,9 @@ wait)
 if __name__ == '__main__':
 
     job_id = str(uuid.uuid4())
-    pp_degree=2
-    dp_degree=128
-    n_layer_per_device=16
+    pp_degree=4
+    dp_degree=64
+    n_layer_per_device=8
     node_size=32
 
     template = template.replace('{{JOB_ID}}', job_id)
