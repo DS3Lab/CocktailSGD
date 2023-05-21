da8fbd04cc4b426aa40cdfd99d0b2ad7faff191d
Jue Wang <zjuwangjue@gmail.com>
2023-05-21 13:09:01 +0000

    update

--- slurm_scripts/BZS_6M_CT.py
+++ slurm_scripts/BZS_6M_CT.py
@@ -11,6 +11,25 @@ template = '''#!/bin/bash
 
 nvidia-smi
 
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/root/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/root/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/root/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/root/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
+if [ -f "/root/miniconda3/etc/profile.d/mamba.sh" ]; then
+    . "/root/miniconda3/etc/profile.d/mamba.sh"
+fi
+# <<< conda initialize <<<
+
 cd /work/data/
 rm -rf CocktailSGD
 git clone https://github.com/DS3Lab/CocktailSGD.git
