445688ba655c03fea2d38c0d6abbaf265db8d06b
Jue Wang <zjuwangjue@gmail.com>
2023-06-09 05:58:44 +0000

    llama script

---
+++ example_scripts/finetune_allreduce_llama7b.sh
@@ -0,0 +1,52 @@
+
+export WANDB_NAME=llama-7b-instruct-single-node
+
+netif=lo
+master_ip=127.0.0.1
+export GLOO_SOCKET_IFNAME=${netif}
+export NCCL_SOCKET_IFNAME=${netif}
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+export QUANT_BITS=4
+export TOPK_RATIO=0.2
+export RANDOMP_RATIO=0.1
+export SHOW_DATA=0
+
+ARGS="--model-name /root/fm/models/llama-7b-shard \
+--tokenizer-name /root/fm/models/llama-7b-shard \
+--project-name demo \
+--model-type flash_llama \
+--optimizer adam \
+--seed 42 \
+--load-pretrained-model true \
+--task-name ni_dehelm:0.2,p3_dehelm:0.2,pile:0.6 \
+--checkpoint-path ./model_ckpts/$WANDB_NAME \
+--num-layers 8 --embedding-dim 4096 \
+--total-steps 10000 --warmup-steps 10 --train-warmup-steps 0 \
+--checkpoint-steps 1000 \
+--lr 1e-5 --seq-length 2048 --batch-size 32 --micro-batch-size 2 --gradient-accumulate-step 1 \
+--dist-url tcp://${master_ip}:7033 \
+--world-size 8 --pipeline-group-size 4 --data-group-size 2 \
+--job-id 0 --net-interface ${netif} \
+--fp16 \
+--dp-backend nccl \
+--dp-mode allreduce \
+--pp-mode gpipe --profiling no-profiling"
+
+(trap 'kill 0' SIGINT; \
+python dist_lm_train.py $(echo ${ARGS}) --cuda-id 0 --rank 0 \
+    & \
+python dist_lm_train.py $(echo ${ARGS}) --cuda-id 1 --rank 1 \
+    & \
+python dist_lm_train.py $(echo ${ARGS}) --cuda-id 2 --rank 2 \
+    & \
+python dist_lm_train.py $(echo ${ARGS}) --cuda-id 3 --rank 3 \
+    & \
+python dist_lm_train.py $(echo ${ARGS}) --cuda-id 4 --rank 4 \
+    & \
+python dist_lm_train.py $(echo ${ARGS}) --cuda-id 5 --rank 5 \
+    & \
+python dist_lm_train.py $(echo ${ARGS}) --cuda-id 6 --rank 6 \
+    & \
+python dist_lm_train.py $(echo ${ARGS}) --cuda-id 7 --rank 7 \
+    & \
+wait)
